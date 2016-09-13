cf428d3e0a4889556cd43be3b9683c9c86bbbc9b
christiandennis <christiandennis11@gmail.com>
2016-09-12 20:32:16 -0700

    finished part 6.Models, Templates and views

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
-
+from django.template.defaultfilters import slugify
 from django.db import models
 
 # Create your models here.
@@ -7,15 +7,18 @@ class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
-
-    class Meta:
-        verbose_name_plural = 'categories'
+    slug = models.SlugField(unique=True)
 
     def __str__(self):
         return self.name
     def __unicode__(self):
         return self.name
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
+    class Meta:
+        verbose_name_plural = 'categories'
 
 class Page(models.Model):
     category = models.ForeignKey(Category)
